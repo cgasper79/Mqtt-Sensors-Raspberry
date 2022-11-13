84b8dbab04d0cd6db3caa56673c07c4262c4bf58
cgasper79 <51058790+cgasper79@users.noreply.github.com>
2022-11-13 21:03:30 +0100

    Fix get_cpu_temp

--- mqtt_monitor.py
+++ mqtt_monitor.py
@@ -13,7 +13,7 @@ def get_cpu_temp():
     tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
     cpu_temp = tempFile.read()
     tempFile.close()
-    return float(tempFile)/1000
+    return float(cpu_temp)/1000
 
 def connect_mqtt():
 
